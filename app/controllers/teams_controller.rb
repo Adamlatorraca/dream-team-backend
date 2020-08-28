cd7f6b42fca9ae66cb4283a21d376aefd449f899
Adamlatorraca <adam.latorraca@gmail.com>
2020-08-28 13:08:45 -0400

    add create, destroy actions and strong params

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,11 +1,28 @@
 class TeamsController < ApplicationController
     def index
         teams = Team.all
-        render json: teams.to_json
+        render json: teams, except: [:created_at, :updated_at]
+    end
+
+    def create
+        team = Team.create(team_params)
+        render json: team, except: [:created_at, :updated_at]
     end
 
     def show
         team = Team.find_by(id: params[:id])
-        render json: team.players.to_json
+        render json: team, include: [:players]
+    end
+
+    def destroy
+        team = Team.find_by(id: params[:id])
+        team.destroy
+        render json: team
+    end
+
+private
+
+    def team_params
+        params.require(:team).permit(:name, :logo, :player_id)
     end
 end
